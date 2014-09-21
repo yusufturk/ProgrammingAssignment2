c53df67eb32f66af04d3c563059b7c437262f2fa
Yusuf Turk <yusufturk2@gmail.com>
2014-09-21 14:15:28 +0300

    - initial commit - without comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera Data Science Specialization - R Programming (rprog-007)
+## Author : Yusuf Turk  <yusufturk2@gmail.com>
 
-## Write a short comment describing this function
+## Function makeCacheMatrix()
+## Creates a matrix x
+## set()                :   replaces the matrix m
+## get()                :   returns the matrix m
+## setInverseMatrix()   :   inverts the matrix 
+## getInverseMatrix()   :   returns the inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setInverseMatrix <- function(solve) m <<- solve
+    getInverseMatrix <- function() m
+    list(set = set, get = get,
+        setInverseMatrix = setInverseMatrix,
+        getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## Function cacheSolve()
+## Returns the inverted matrix of a given matrix x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    myMatrix <- x$getInverseMatrix()
+    if(!is.null(myMatrix)){
+        message("getting cached matrix data")
+        return(myMatrix)
+    }
+    matrixData <- x$get()
+    myMatrix <- solve(matrixData)
+    x$setInverseMatrix(myMatrix)
+    myMatrix
 }
